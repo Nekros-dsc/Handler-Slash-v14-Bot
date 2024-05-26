8dc5afb688157bd9ed9ee75b39976d67f6678999
nekrxs <122643715+Nekros-dsc@users.noreply.github.com>
2024-05-26 17:05:46 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    clients: {
-        token: 'put-token-here', //token
-        name: 'Nova World ❤️', //nom footer
-        logo: '', //logo footer
-        activity: '/novaworld', //statut
-        embedColor: 'Random' //couleur embed
-    }
-}
